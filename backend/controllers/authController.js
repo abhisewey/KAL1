1fd8240f0a25959db77969a98492fe81f346962a
abhisewey <abhishekem012@gmail.com>
2025-11-28 11:24:37 +0530

    backenddone

---
+++ backend/controllers/authController.js
@@ -0,0 +1,70 @@
+import bcrypt from "bcryptjs";
+import User from "../models/userModel.js";
+import {generateToken} from "../utils/generateToken.js";
+
+// ----------------- SIGNUP -----------------
+export const registerUser = async (req, res) => {
+  try {
+    const { username, email, password, profilePic } = req.body;
+
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "All fields required" });
+    }
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "Email already registered" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(password, salt);
+
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPass,
+      profilePic,
+    });
+
+    return res.json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      profilePic: user.profilePic,
+      token: generateToken(user._id),
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// ----------------- LOGIN -----------------
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user)
+      return res.status(400).json({ message: "Invalid email or password" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid email or password" });
+
+      return res.json({
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        profilePic: user.profilePic,
+        token: generateToken(user),
+      });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// ----------------- GET PROFILE -----------------
+export const getProfile = async (req, res) => {
+  const user = await User.findById(req.user.id).select("-password");
+  res.json(user);
+};
